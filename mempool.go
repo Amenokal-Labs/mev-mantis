6021c993d352e940691520b6b9ae6640ccc2d494
18a6 <ar.brhl.49@gmail.com>
2023-06-29 10:59:22 +0100

    fix: remamed getMempool() to main()

--- mempool.go
+++ mempool.go
@@ -1,6 +1,6 @@
 package main
 
-func getMempool() {
+func main() {
 	HandleTransaction("POST", "0x936ed523582d8d531eeef8c2ae7e478858f1b90c568badc95a581ea61267110f")
 	HandleTransaction("DELETE", "0x936ed523582d8d531eeef8c2ae7e478858f1b90c568badc95a581ea61267110f")
 
