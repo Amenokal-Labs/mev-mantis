04993f43a6ce8e8e86b181edc2ae31f55af99fc3
18a6 <ar.brhl49@gmail.com>
2023-07-04 04:22:28 +0100

    feat: rounded prices

--- arbitrage.go
+++ arbitrage.go
@@ -21,7 +21,7 @@ func main() {
 	}
 	fmt.Println("we have a connection")
 
-	const V2PairAddress string = "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852" // WETH/USDT
+	const V2PairAddress string = "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc" // WETH/USDT
 	cmnV2PairAddress := common.HexToAddress(V2PairAddress)
 	v2PairContract, err := UniswapV2Pair.NewUniswapV2Pair(cmnV2PairAddress, client)
 	if err != nil {
@@ -29,7 +29,13 @@ func main() {
 	}
 
 	data, err := v2PairContract.GetReserves(&bind.CallOpts{})
-	fmt.Println(new(big.Int).Div(data.Reserve1, data.Reserve0))
+
+	price0 := new(big.Float).Quo(new(big.Float).SetInt(data.Reserve1), new(big.Float).SetInt(data.Reserve0))
+
+	adjustmentFactor0 := new(big.Int).Exp(big.NewInt(10), big.NewInt(12), nil)
+	adjustedPrice0 := new(big.Float).Quo(price0, new(big.Float).SetInt(adjustmentFactor0))
+	roundedPrice0 := fmt.Sprintf("V2 Price: 1 USDC = %.10f ETH", adjustedPrice0)
+	fmt.Println(roundedPrice0)
 
 	const V3PairAddress string = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
 	cmnV3PairAddress := common.HexToAddress(V3PairAddress)
@@ -39,18 +45,20 @@ func main() {
 	}
 
 	data2, err := v3PairContract.Slot0(&bind.CallOpts{})
-	fmt.Println("SqrtPriceX96:", data2.SqrtPriceX96)
+	// fmt.Println("SqrtPriceX96:", data2.SqrtPriceX96)
 
 	q := new(big.Int).Exp(big.NewInt(2), big.NewInt(96), nil)
-	fmt.Println("Q96:", q)
+	// fmt.Println("Q96:", q)
 
 	sqrtPrice := new(big.Float).Quo(new(big.Float).SetInt(data2.SqrtPriceX96), new(big.Float).SetInt(q))
-	fmt.Println("sqrtPrice:", sqrtPrice)
+	// fmt.Println("sqrtPrice:", sqrtPrice)
 
 	price := new(big.Float).Mul(sqrtPrice, sqrtPrice)
-	fmt.Println("Price:", price)
+	// fmt.Println("Price:", price)
 
 	adjustmentFactor := new(big.Int).Exp(big.NewInt(10), big.NewInt(12), nil)
 	adjustedPrice := new(big.Float).Quo(price, new(big.Float).SetInt(adjustmentFactor))
-	fmt.Println("actual Price:", adjustedPrice)
+
+	roundedPrice := fmt.Sprintf("V3 Price: 1 USDC = %.10f ETH", adjustedPrice)
+	fmt.Println(roundedPrice)
 }
