271c8b0efd3984218c1f440049df4cf279d05d31
18a6 <ar.brhl49@gmail.com>
2023-07-10 14:27:56 +0100

    fix: failed to SubscribePendingTransactions

--- cmd/GF/main.go
+++ cmd/GF/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/Amenokal-Labs/mev-mantis.git/pkg/utils"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient/gethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
 func main() {
@@ -13,9 +17,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	_ = client
 	fmt.Println("client connected")
 
 	test, err := client.PendingTransactionCount(context.Background())
 	fmt.Println(test)
+
+	rpc, err := rpc.Dial("https://mainnet.infura.io/ws/v3/" + utils.GetAPIKey("INFURA_KEY"))
+	client2 := gethclient.New(rpc)
+
+	hashes := make(chan common.Hash)
+	_, err = client2.SubscribePendingTransactions(context.Background(), hashes)
+
+	if err != nil {
+		log.Fatal("failed to subscribe", err)
+	}
+
+	select {
+	case hash := <-hashes:
+		fmt.Println("")
+		_ = hash
+	}
 }
