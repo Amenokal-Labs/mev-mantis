e6112869d6bdea79e0a1fae3bab5d250ffe90ec9
18a6 <ar.brhl49@gmail.com>
2023-07-13 09:53:50 +0100

    feat: got tx input and tx contract

--- cmd/frontrunning/main.go
+++ cmd/frontrunning/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/Amenokal-Labs/mev-mantis.git/pkg/utils"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/ethclient/gethclient"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -20,8 +23,11 @@ func main() {
 	}
 
 	rpc, err := rpc.Dial("wss://mainnet.infura.io/ws/v3/" + utils.GetAPIKey("INFURA_KEY"))
-	rpcClient := gethclient.New(rpc)
-	fmt.Println("client connected")
+	if err != nil {
+		panic(err)
+	}
+	subscriber := gethclient.New(rpc)
+	fmt.Println("client connected..")
 
 	// txs := make(chan *types.Transaction)
 	// _, err = rpcClient.SubscribeFullPendingTransactions(context.Background(), txs)
@@ -30,36 +36,71 @@ func main() {
 	// }
 
 	hashes := make(chan common.Hash)
-	_, err = rpcClient.SubscribePendingTransactions(context.Background(), hashes)
+	_, err = subscriber.SubscribePendingTransactions(context.Background(), hashes)
 	if err != nil {
 		panic(err)
 	}
 
 	for {
-		pendingCount, _ := httpsClient.PendingTransactionCount(context.Background())
-		fmt.Println("Pending count:", pendingCount)
+		pc, _ := httpsClient.PendingTransactionCount(context.Background())
+		fmt.Println("\nPending count:", pc)
 
 		hash := <-hashes
-		fmt.Println(hash)
-		tx, _, _ := httpsClient.TransactionByHash(context.Background(), hash)
-
-		// tx, isPending, err := httpsClient.TransactionByHash(context.Background(), hash)
+		fmt.Println("Tx hash:", hash)
+		txn, _, err := httpsClient.TransactionByHash(context.Background(), hash)
 		// if err != nil {
-		// 	log.Fatal(err)
+		// 	panic(err)
 		// }
-		// fmt.Println(isPending)
-		// if isPending != true {
-		// 	log.Fatal("tx not pending anymore")
-		// }
-		data, _ := tx.MarshalJSON()
-		// fmt.Println(string(data))
+		data, err := txn.MarshalJSON()
+		if err != nil {
+			panic(err)
+		}
+
+		from, err := types.Sender(types.NewLondonSigner(txn.ChainId()), txn)
+		if err != nil {
+			panic(err)
+		}
 
 		type Tx struct {
 			Input string `json:"input"`
 		}
 		var tx0 Tx
 		json.Unmarshal(data, &tx0)
-		fmt.Println("\n\ntx input: ", tx0.Input)
-		time.Sleep(10 * time.Second)
+		fmt.Println("tx input: ", tx0.Input)
+
+		// get contract code if any
+		type Contract struct {
+			JsonRpc string `json:"jsonrpc"`
+			Id      int    `json:"id"`
+			Result  string `json:"result"`
+		}
+		body := []byte(`{
+			"jsonrpc":"2.0",
+			"method":"eth_getCode",
+			"params": ["` + from.Hex() + `", "pending"],
+			"id":1
+		}`)
+		r, err := http.NewRequest("POST", "https://mainnet.infura.io/v3/"+utils.GetAPIKey("INFURA_KEY"), bytes.NewBuffer(body))
+		if err != nil {
+			panic(err)
+		}
+		r.Header.Add("Content-Type", "application/json")
+		client := &http.Client{}
+		res, err := client.Do(r)
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
+		contract := &Contract{}
+		derr := json.NewDecoder(res.Body).Decode(contract)
+		if derr != nil {
+			panic(derr)
+		}
+		fmt.Println("\nContract code:", contract.Result)
+		// if res.StatusCode != http.StatusCreated {
+		// 	panic(res.Status)
+		// }
+		time.Sleep(4 * time.Second)
+
 	}
 }
