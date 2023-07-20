023859740b3ccc4768acb94b2a1548e436f9a279
18a6 <ar.brhl49@gmail.com>
2023-07-20 16:15:53 +0100

    fix: formatted code into meaningful functions

--- cmd/frontrunning/main.go
+++ cmd/frontrunning/main.go
@@ -43,83 +43,24 @@ func main() {
 	// 	panic(err)
 	// }
 
-	hashes := make(chan common.Hash)
-	_, err = subscriber.SubscribePendingTransactions(context.Background(), hashes)
-	if err != nil {
-		log.Fatal("[2] ", err)
-	}
-
 	for {
 		pc, _ := ethclient.PendingTransactionCount(context.Background())
 		fmt.Println("\nPending count:", pc)
 
-		hash := <-hashes
-		fmt.Println("      Tx hash:", hash)
-		txn, _, err := ethclient.TransactionByHash(context.Background(), hash)
-		if err != nil {
-			log.Fatal("[3] ", err)
-		}
-
-		marshalledTxn, err := txn.MarshalJSON()
-		if err != nil {
-			log.Fatal("[4] ", err)
-		}
-
-		from, err := types.Sender(types.NewLondonSigner(txn.ChainId()), txn)
-		if err != nil {
-			log.Fatal("[5] ", err)
-		}
-
-		type Tx struct {
+		txn, marshalledTxn, from := getTxn(ethclient, subscriber)
+		type Txn struct {
 			Input string `json:"input"`
 		}
-		var tx Tx
+		var tx Txn
 		json.Unmarshal(marshalledTxn, &tx)
 		fmt.Println("  Tx Calldata:", tx.Input)
 		fmt.Println("   Tx address:", txn.To())
 
-		// get contract code if any
-		type Contract struct {
-			JsonRpc string `json:"jsonrpc"`
-			Id      int    `json:"id"`
-			Result  string `json:"result"`
-		}
-		reciever := txn.To()
-		if reciever == nil {
-			log.Fatal("[14] transaction is a contract creation.")
-		}
-		body := []byte(`{
-			"jsonrpc":"2.0",
-			"method":"eth_getCode",
-			"params": ["` + reciever.String() + `", "pending"],
-			"id":1
-		}`)
-		r, err := http.NewRequest("POST", "https://mainnet.infura.io/v3/"+utils.GetKey("INFURA_KEY"), bytes.NewBuffer(body))
-		if err != nil {
-			log.Fatal("[6] ", err)
-		}
-		r.Header.Add("Content-Type", "application/json")
-		client := &http.Client{}
-		res, err := client.Do(r)
-		if err != nil {
-			log.Fatal("[7] ", err)
-		}
-		defer res.Body.Close()
-		contract := &Contract{}
-		derr := json.NewDecoder(res.Body).Decode(contract)
-		if derr != nil {
-			log.Fatal("[8] ", derr)
-		}
-		// fmt.Println("\nContract code:", contract.Result)
-		// if res.StatusCode != http.StatusCreated {
-		// 	panic(res.Status)
-		// }
-
-		if contract.Result == "0x" {
+		contract := getContract(txn)
+		if contract == "0x" {
 			continue
 		}
-		contractCode := contract.Result
-		fmt.Println("\nContract code:", contractCode[:26], "...")
+		fmt.Println("\nContract code:", contract[:26], "...")
 
 		calldata := tx.Input
 		address := from.String()
@@ -130,6 +71,70 @@ func main() {
 		fmt.Println("___________________________")
 		time.Sleep(4 * time.Second)
 	}
+}
+
+func getTxn(ethclient *ethclient.Client, subscriber *gethclient.Client) (*types.Transaction, []byte, common.Address) {
+	hashes := make(chan common.Hash)
+	_, err := subscriber.SubscribePendingTransactions(context.Background(), hashes)
+	if err != nil {
+		log.Fatal("[2] ", err)
+	}
+	hash := <-hashes
+	fmt.Println("      Tx hash:", hash)
+	txn, _, err := ethclient.TransactionByHash(context.Background(), hash)
+	if err != nil {
+		log.Fatal("[3] ", err)
+	}
+
+	marshalledTxn, err := txn.MarshalJSON()
+	if err != nil {
+		log.Fatal("[4] ", err)
+	}
+
+	from, err := types.Sender(types.NewLondonSigner(txn.ChainId()), txn)
+	if err != nil {
+		log.Fatal("[5] ", err)
+	}
+
+	return txn, marshalledTxn, from
+}
+
+func getContract(txn *types.Transaction) string {
+	type Contract struct {
+		JsonRpc string `json:"jsonrpc"`
+		Id      int    `json:"id"`
+		Result  string `json:"result"`
+	}
+	to := txn.To()
+	if to == nil {
+		log.Fatal("[14] transaction is a contract creation.")
+	}
+	body := []byte(`{
+		"jsonrpc":"2.0",
+		"method":"eth_getCode",
+		"params": ["` + to.String() + `", "pending"],
+		"id":1
+	}`)
+	r, err := http.NewRequest("POST", "https://mainnet.infura.io/v3/"+utils.GetKey("INFURA_KEY"), bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal("[6] ", err)
+	}
+	r.Header.Add("Content-Type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		log.Fatal("[7] ", err)
+	}
+	defer res.Body.Close()
+	contract := &Contract{}
+	derr := json.NewDecoder(res.Body).Decode(contract)
+	if derr != nil {
+		log.Fatal("[8] ", derr)
+	}
+	// if res.StatusCode != http.StatusCreated {
+	// 	panic(res.Status)
+	// }
+	return contract.Result
 }
 
 func replaceAddress(_calldata, _address string) string {
